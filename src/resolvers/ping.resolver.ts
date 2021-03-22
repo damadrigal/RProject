99e6e3bdd26571ad9bb04e157711349bc92c4128
Ruben Mora Vargas <ruben.mora.vargas@una.cr>
2021-03-21 19:01:19 -0600

    Add a simple base for refactoring and improving

---
+++ src/resolvers/ping.resolver.ts
@@ -0,0 +1,16 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class PingResolver{
+    @Query(()=>String)
+    ping(){
+        return "Pong!";
+    }
+    @Mutation(() => String)
+    async hello(
+        @Arg("name") name: string) {
+        return `Hello my friend ${name}`;
+    }
+}
+
+
